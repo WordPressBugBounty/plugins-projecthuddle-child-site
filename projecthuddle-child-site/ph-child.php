a5d6667253fb075385bdad5a58934c63e8b35511
Sean Murphy <sean@iamseanmurphy.com>
2025-04-15 07:17:28 +0000

    1.2.7

--- projecthuddle-child-site/ph-child.php
+++ projecthuddle-child-site/ph-child.php
@@ -5,10 +5,10 @@
  * Description: Collect note-style feedback from your client’s websites and sync them with your SureFeedback parent project.
  * Author: Brainstorm Force
  * Author URI: https://www.brainstormforce.com
- * Version: 1.2.6
+ * Version: 1.2.7
  *
  * Requires at least: 4.7
- * Tested up to: 6.7.1
+ * Tested up to: 6.8
  *
  * Text Domain: ph-child
  * Domain Path: languages
